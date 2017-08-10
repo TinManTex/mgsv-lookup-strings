18599ebcebb816259e850aad52aceb954c4076de
TinManTex <tinman.squad@gmail.com>
2017-08-10 19:41:52 +1200

    Initial commit

---
+++ GameDir/mod/modules/IHStringsManual.lua
@@ -0,0 +1,13 @@
+--tex manualy guessed
+local this={}
+
+this.lookupStrings={
+  "StartLogin",--messageId for a Network messageClass message (guessed, and confirmed, from EndLogin)
+  "MotherBaseStage",--messageClass, messages that use it only defined in mother base scripts (obviously), but still fires before 
+  "TYPE_VEHICLE",--markerType
+  --tre2 files
+  "heightFormat",
+  "comboTextureFormat",
+}
+
+return this
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Various lookup strings mostly unfilered string scrapes from exe,fox2 etc.
+Used in InfLookup for various debug print purposes to attempt to match StrCode32 hashes.
+Heavily used in the by debugMessages option.
