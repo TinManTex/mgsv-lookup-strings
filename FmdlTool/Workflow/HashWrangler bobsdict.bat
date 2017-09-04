8412f75c2f9bfbeefb4fbed31f676161104d69bd
TinManTex <tinman.squad@gmail.com>
2017-09-04 18:59:23 +1200

    more hashes

--- FmdlTool/Workflow/HashWrangler bobsdict.bat
+++
@@ -1,2 +0,0 @@
-D:\GitHub\HashWrangler\HashWrangler\bin\Debug\HashWrangler.exe "D:\GitHub\mgsv-lookup-strings\FmdlTool\Hashes\StrCode64\Tpp\All_unique.txt" "D:\GitHub\mgsv-lookup-strings\FmdlTool\Dictionaries\fmdl_dictionary-test\fmdl_dictionary-bobs.txt" -HashFunction StrCode64
-pause
