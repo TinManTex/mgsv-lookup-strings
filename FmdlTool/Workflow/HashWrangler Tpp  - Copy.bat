f0aed4933488de1c6d31e8fd4022b132a533cb2e
TinManTex <tinman.squad@gmail.com>
2017-09-08 21:37:16 +1200

    gz format fmdl strings path reconstruction

---
+++ FmdlTool/Workflow/HashWrangler Tpp  - Copy.bat
@@ -0,0 +1,2 @@
+D:\GitHub\HashWrangler\HashWrangler\bin\Debug\HashWrangler.exe "D:\Projects\MGS\!SubMods\ModelSwaps\pfs\pfs0_main0_def.fmdl_textureHashes.txt" "D:\GitHub\mgsv-lookup-strings\FmdlTool\Dictionaries\texture_dictionary-test" -HashFunction PathCode64
+pause
