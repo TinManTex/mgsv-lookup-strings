4f595fc18dac742913bcca6c772ba43d79bbd5dd
TinManTex <tinman.squad@gmail.com>
2018-02-05 14:45:32 +1300

    More cleanup

--- SubpTool/Workflow/HashWrangler test.bat
+++
@@ -1,2 +0,0 @@
-D:\GitHub\HashWrangler\HashWrangler\bin\Debug\HashWrangler.exe "D:\GitHub\mgsv-lookup-strings\SubpTool\Hashes\tpp_pc_1.0.11.txt" "D:\GitHub\mgsv-lookup-strings\SubpTool\Dictionaries\subp_dictionary-test" -HashFunction StrCode32
-pause
