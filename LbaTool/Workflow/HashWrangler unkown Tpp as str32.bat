8dd3e5e5eb6148f3852107b7589f65cfe21f863b
TinManTex <tinman.squad@gmail.com>
2018-02-05 13:44:27 +1300

    Cleanup

--- LbaTool/Workflow/HashWrangler unkown Tpp as str32.bat
+++
@@ -1,2 +0,0 @@
-D:\GitHub\HashWrangler\HashWrangler\bin\Debug\HashWrangler.exe "D:\GitHub\mgsv-lookup-strings\LbaTool\Hashes\Tpp\unknown" "D:\GitHub\mgsv-lookup-strings\LbaTool\Dictionaries\lba_dictionary-testall" -HashFunction strcode32
-pause
