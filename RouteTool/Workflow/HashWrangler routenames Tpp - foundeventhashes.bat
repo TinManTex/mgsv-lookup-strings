2f0449d66eff9cf097af52a347b81a5b053ef210
TinManTex <tinman.squad@gmail.com>
2018-02-10 08:29:07 +1300

    route research: params in hex

---
+++ RouteTool/Workflow/HashWrangler routenames Tpp - foundeventhashes.bat
@@ -0,0 +1,4 @@
+del J:\GameData\MGS\filetypecrushed\frt\*.txt
+"D:\GitHub\FoxLibDumper\FoxLibDumper\bin\Debug\FoxLibDumper.exe" J:\GameData\MGS\filetypecrushed\frt
+D:\GitHub\HashWrangler\HashWrangler\bin\Debug\HashWrangler.exe "J:\GameData\MGS\filetypecrushed\frt" "D:\GitHub\mgsv-lookup-strings\RouteTool\Dictionaries\RouteNames" -HashFunction StrCode32
+pause
